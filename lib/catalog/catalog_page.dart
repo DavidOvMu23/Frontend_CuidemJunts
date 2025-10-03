eb6df6407baecdacc47ca418f4f2a5da3fec4212
David O <david23oviedo@gmail.com>
2025-10-03 20:40:20 +0200

    estructura de carpetas en CLEAN ARCHITECTURE

---
+++ lib/catalog/catalog_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CatalogPage extends StatelessWidget {
+  const CatalogPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Catálogo de Componentes')),
+      body: ListView(
+        children: const [
+          //PONER AQUÍ LOS WIDGETS
+        ],
+      ),
+    );
+  }
+}
