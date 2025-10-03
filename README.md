7febf4a63f0d37bd4f316be7610f44d6195b9759
David O <david23oviedo@gmail.com>
2025-10-03 19:44:22 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# frontend_cuidemjunts
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/frontend_cuidemjunts/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.frontend_cuidemjunts
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
