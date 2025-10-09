91d4872d3427a6e1672237eab7c7a1a0df7926c9
David O <david23oviedo@gmail.com>
2025-10-09 15:34:59 +0200

    Agregar archivos de demostración y plantilla de widgets al catálogo(david)

--- lib/catalog/catalog_page.dart
+++ lib/catalog/catalog_page.dart
@@ -1,17 +1,1 @@
-import 'package:flutter/material.dart';
 
-class CatalogPage extends StatelessWidget {
-  const CatalogPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Catálogo de Componentes')),
-      body: ListView(
-        children: const [
-          //PONER AQUÍ LOS WIDGETS
-        ],
-      ),
-    );
-  }
-}
