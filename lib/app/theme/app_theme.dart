d49d011fed6ffe8a8d6c4bd1f086eacea9857945
DavidOvMu23 <david23oviedo@gmail.com>
2025-10-17 21:54:27 +0200

    Mejorar la estructura y comentarios del tema de la aplicación, optimizando la definición de estilos para el modo claro y oscuro.(david)

--- lib/app/theme/app_theme.dart
+++ lib/app/theme/app_theme.dart
@@ -5,130 +5,173 @@ import 'package:frontend_cuidemjunts/app/theme/app_palette.dart';
 /// Me lo ha hecho el chat a partir de las constantes que he definido
 /// en el app palette (lo siento pepe)
 class AppTheme {
-  //TEMA CLARO -------------------------------------------
+  // MODO CLARO -------------------------------------------
   static final ThemeData lightTheme = ThemeData(
-    useMaterial3: true, // Usa el sistema de diseño Material 3
-    brightness: Brightness.light, // Modo claro
-    scaffoldBackgroundColor:
-        AppPalette.backgroundLight, // Color de fondo principal
-    primaryColor: AppPalette.primaryLight, // Color primario
+    useMaterial3: true,
+    brightness: Brightness.light,
+    scaffoldBackgroundColor: AppPalette.backgroundLight,
+    primaryColor: AppPalette.primaryLight,
     colorScheme: const ColorScheme.light(
-      background: AppPalette.backgroundLight, // Fondo base
-      surface: AppPalette.surfaceLight, // Superficie (tarjetas, appbars, etc.)
-      primary: AppPalette.primaryLight, // Color principal
-      onPrimary: AppPalette
-          .onPrimaryLight, // Color del texto/iconos sobre el color primario
-      secondary: AppPalette.accentLight, // Color secundario (acentos)
-      error: AppPalette.errorLight, // Color de error
-      onError: Colors.white, // Color del texto sobre el color de error
+      surface: AppPalette.surfaceLight,
+      primary: AppPalette.primaryLight,
+      onPrimary: AppPalette.onPrimaryLight,
+      secondary: AppPalette.accentLight,
+      error: AppPalette.errorLight,
+      onError: AppPalette.errorFontLight,
     ),
-    cardColor: AppPalette.cardLight, // Color de las tarjetas
-    appBarTheme: const AppBarTheme(
-      backgroundColor: AppPalette.surfaceLight, // Fondo del AppBar
-      foregroundColor:
-          AppPalette.textPrimaryLight, // Color del texto/iconos del AppBar
-      elevation: 0, // Sin sombra
-      centerTitle: true, // Centra el título
-      titleTextStyle: TextStyle(
-        color: AppPalette.textPrimaryLight, // Color del texto del título
-        fontSize: 20, // Tamaño del texto
-        fontWeight: FontWeight.w600, // Grosor medio
-      ),
-    ),
-    textTheme: const TextTheme(
-      headlineLarge: TextStyle(
-        color: AppPalette.textPrimaryLight, // Texto principal (títulos grandes)
-        fontWeight: FontWeight.bold,
-        fontSize: 24,
-      ),
-      titleMedium: TextStyle(
-        color: AppPalette.textSecondaryLight, // Subtítulos
-        fontWeight: FontWeight.w600,
-        fontSize: 18,
-      ),
-      bodyMedium: TextStyle(
-        color: AppPalette.textBodyLight, // Texto del cuerpo principal
-        fontSize: 16,
-      ),
-      bodySmall: TextStyle(
-        color: AppPalette
-            .textDescriptionLight, // Descripciones o textos secundarios
-        fontSize: 14,
-      ),
-      labelSmall: TextStyle(
-        color: AppPalette.textFootnoteLight, // Notas o pies de texto
-        fontSize: 12,
-      ),
-    ),
-    iconTheme: const IconThemeData(
-      color: AppPalette.primaryLight, // Color por defecto de los iconos
-    ),
-    floatingActionButtonTheme: const FloatingActionButtonThemeData(
-      backgroundColor: AppPalette.primaryLight, // Color del FAB
-      foregroundColor: Colors.white, // Color del ícono del FAB
-    ),
-  );
+    cardColor: AppPalette.cardLight,
 
-  // ============================================================
-  //                     🌙 TEMA OSCURO
-  // ============================================================
-  static final ThemeData darkTheme = ThemeData(
-    useMaterial3: true, // Usa Material 3
-    brightness: Brightness.dark, // Modo oscuro
-    scaffoldBackgroundColor:
-        AppPalette.backgroundDark, // Fondo principal oscuro
-    primaryColor: AppPalette.primaryDark, // Color primario oscuro
-    colorScheme: const ColorScheme.dark(
-      background: AppPalette.backgroundDark, // Fondo base oscuro
-      surface: AppPalette.surfaceDark, // Superficies oscuras
-      primary: AppPalette.primaryDark, // Color primario
-      onPrimary: AppPalette.onPrimaryDark, // Texto sobre primario
-      secondary: AppPalette.accentDark, // Color secundario
-      error: AppPalette.errorDark, // Color de error
-      onError: Colors.white, // Texto sobre color de error
-    ),
-    cardColor: AppPalette.cardDark, // Color de las tarjetas
+    // AppBar
     appBarTheme: const AppBarTheme(
-      backgroundColor: AppPalette.surfaceDark, // Fondo del AppBar
-      foregroundColor: AppPalette.textPrimaryDark, // Color del texto/iconos
-      elevation: 0, // Sin sombra
-      centerTitle: true, // Centra el título
+      backgroundColor: AppPalette.surfaceLight,
+      foregroundColor: AppPalette.textPrimaryLight,
+      elevation: 0,
+      centerTitle: true,
       titleTextStyle: TextStyle(
-        color: AppPalette.textPrimaryDark, // Texto del título
+        color: AppPalette.textPrimaryLight,
         fontSize: 20,
         fontWeight: FontWeight.w600,
       ),
     ),
+
+    // Textos
     textTheme: const TextTheme(
       headlineLarge: TextStyle(
-        color: AppPalette.textPrimaryDark, // Títulos grandes
+        color: AppPalette.textPrimaryLight,
         fontWeight: FontWeight.bold,
         fontSize: 24,
       ),
       titleMedium: TextStyle(
-        color: AppPalette.textSecondaryDark, // Subtítulos
+        color: AppPalette.textSecondaryLight,
         fontWeight: FontWeight.w600,
         fontSize: 18,
       ),
-      bodyMedium: TextStyle(
-        color: AppPalette.textBodyDark, // Texto principal
-        fontSize: 16,
-      ),
+      bodyMedium: TextStyle(color: AppPalette.textBodyLight, fontSize: 16),
       bodySmall: TextStyle(
-        color: AppPalette.textDescriptionDark, // Descripciones
+        color: AppPalette.textDescriptionLight,
         fontSize: 14,
       ),
-      labelSmall: TextStyle(
-        color: AppPalette.textFootnoteDark, // Notas o pies de texto
-        fontSize: 12,
+      labelSmall: TextStyle(color: AppPalette.textFootnoteLight, fontSize: 12),
+    ),
+
+    // Botones principales
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: AppPalette.primaryLight,
+        foregroundColor: Colors.white,
+        textStyle: const TextStyle(fontWeight: FontWeight.w600),
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+        ),
+        elevation: 1.5,
       ),
     ),
-    iconTheme: const IconThemeData(
-      color: AppPalette.primaryDark, // Color de los iconos
+
+    // Botones tonales
+    filledButtonTheme: FilledButtonThemeData(
+      style: FilledButton.styleFrom(
+        backgroundColor: AppPalette.accentLight,
+        foregroundColor: AppPalette.textPrimaryLight,
+        textStyle: const TextStyle(fontWeight: FontWeight.w500),
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+        ),
+      ),
     ),
+
+    // Iconos
+    iconTheme: const IconThemeData(color: AppPalette.primaryLight),
+
+    // FAB
     floatingActionButtonTheme: const FloatingActionButtonThemeData(
-      backgroundColor: AppPalette.primaryDark, // Fondo del FAB
-      foregroundColor: Colors.white, // Ícono del FAB
+      backgroundColor: AppPalette.primaryLight,
+      foregroundColor: Colors.white,
+    ),
+
+    // Estados personalizados
+    snackBarTheme: const SnackBarThemeData(
+      backgroundColor: AppPalette.surfaceLight,
+      contentTextStyle: TextStyle(color: AppPalette.textPrimaryLight),
+    ),
+  );
+
+  // MODO OSCURO -----------------------------------------
+  static final ThemeData darkTheme = ThemeData(
+    useMaterial3: true,
+    brightness: Brightness.dark,
+    scaffoldBackgroundColor: AppPalette.backgroundDark,
+    primaryColor: AppPalette.primaryDark,
+    colorScheme: const ColorScheme.dark(
+      surface: AppPalette.surfaceDark,
+      primary: AppPalette.primaryDark,
+      onPrimary: AppPalette.onPrimaryDark,
+      secondary: AppPalette.accentDark,
+      error: AppPalette.errorDark,
+      onError: AppPalette.errorFontDark,
+    ),
+    cardColor: AppPalette.cardDark,
+
+    // AppBar
+    appBarTheme: const AppBarTheme(
+      backgroundColor: AppPalette.surfaceDark,
+      foregroundColor: AppPalette.textPrimaryDark,
+      elevation: 0,
+      centerTitle: true,
+      titleTextStyle: TextStyle(
+        color: AppPalette.textPrimaryDark,
+        fontSize: 20,
+        fontWeight: FontWeight.w600,
+      ),
+    ),
+
+    // Textos
+    textTheme: const TextTheme(
+      headlineLarge: TextStyle(
+        color: AppPalette.textPrimaryDark,
+        fontWeight: FontWeight.bold,
+        fontSize: 24,
+      ),
+      titleMedium: TextStyle(
+        color: AppPalette.textSecondaryDark,
+        fontWeight: FontWeight.w600,
+        fontSize: 18,
+      ),
+      bodyMedium: TextStyle(color: AppPalette.textBodyDark, fontSize: 16),
+      bodySmall: TextStyle(color: AppPalette.textDescriptionDark, fontSize: 14),
+      labelSmall: TextStyle(color: AppPalette.textFootnoteDark, fontSize: 12),
+    ),
+
+    // Botones principales
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: AppPalette.primaryDark,
+        foregroundColor: Colors.white,
+        textStyle: const TextStyle(fontWeight: FontWeight.w600),
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+        ),
+      ),
+    ),
+
+    // Botones tonales
+    filledButtonTheme: FilledButtonThemeData(
+      style: FilledButton.styleFrom(
+        backgroundColor: AppPalette.accentDark,
+        foregroundColor: AppPalette.textPrimaryDark,
+        textStyle: const TextStyle(fontWeight: FontWeight.w500),
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+        ),
+      ),
+    ),
+
+    // Iconos
+    iconTheme: const IconThemeData(color: AppPalette.primaryDark),
+
+    // FAB
+    floatingActionButtonTheme: const FloatingActionButtonThemeData(
+      backgroundColor: AppPalette.primaryDark,
+      foregroundColor: Colors.white,
     ),
   );
 }
